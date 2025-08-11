43f725d3b3890fffc06d3607d75b9f0a82e62e31
Sergio Ferreira <sergio.ferreira@ctw.bmwgroup.com>
2025-08-11 14:29:21 +0100

    removed C# core functions

--- lib/core/csharp/tests.sh
+++ lib/core/csharp/tests.sh
@@ -3,7 +3,7 @@
 function get_total_tests() {
     local start=$(date +%s%N)
 
-    testCount=$(find_text_in_csharp_test '[TestMethod]' "$DIR" "$BASE_BRANCH" "$CURRENT_BRANCH")
+    testCount=$(find_text_in_test '[TestMethod]' "$DIR" "$BASE_BRANCH" "$CURRENT_BRANCH")
 
     totalTestsExecutionTime=$((($(date +%s%N) - start) / 1000000))
 
