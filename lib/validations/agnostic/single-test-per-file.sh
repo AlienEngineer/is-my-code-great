35a77cd8ac4f6f9af9a36e02f1ccd752ece57fc5
Sergio Ferreira <sergio.ferreira@ctw.bmwgroup.com>
2025-08-18 09:10:14 +0100

    fixed single-test-per-file to account for the multiple test patterns

--- lib/validations/agnostic/single-test-per-file.sh
+++ lib/validations/agnostic/single-test-per-file.sh
@@ -6,14 +6,16 @@ function get_count_test_per_file() {
   local total=0
   for file in $files; do
     [[ -f "$file" ]] || continue
+    local c=0
     for pattern in "${TEST_FUNCTION_PATTERNS[@]}"; do
-      c=$(grep -Fo "$pattern" "$file" | wc -l); 
-      if [[ "$c" -eq 1 ]] then 
-        total=$((total+1))
-        file_name="$(basename "$file")"
-        add_details "$file:0: $file_name"
-      fi
+      found=$(grep -Fo "$pattern" "$file" | wc -l);   
+      c=$((c + found))
     done
+    if [[ "$c" -eq 1 ]] then 
+      total=$((total+1))
+      file_name="$(basename "$file")"
+      add_details "$file:0: $file_name"
+    fi
   done
 
   echo "$total"
