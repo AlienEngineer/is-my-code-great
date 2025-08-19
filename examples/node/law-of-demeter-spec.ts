732d684f21b26a034573e171eb821fbf0799a810
Sergio Ferreira <sergio.ferreira@ctw.bmwgroup.com>
2025-08-19 18:53:52 +0100

    added node tests

---
+++ examples/node/law-of-demeter-spec.ts
@@ -0,0 +1,19 @@
+describe('something', () {
+    it('law of demeter test', () {
+        // This is my test
+        // This is my test
+        // This is my test
+        // This is my test
+        // This is my test
+        something.otherthing.and.Another
+    });
+
+    it('small test', () {
+        // This is my test
+        // This is my test
+        // This is my test
+        // This is my test
+        // This is my test
+        // This is my test
+    });
+});
---
+++ examples/node/verify-spec.ts
@@ -0,0 +1,19 @@
+describe('something', () {
+    it('verify test', () {
+        // This is my test
+        // This is my test
+        // This is my test
+        // This is my test
+        // This is my test
+        expect(Logger.logError).toHaveBeenCalled();
+    });
+
+    it('small test', () {
+        // This is my test
+        // This is my test
+        // This is my test
+        // This is my test
+        // This is my test
+        // This is my test
+    });
+});
