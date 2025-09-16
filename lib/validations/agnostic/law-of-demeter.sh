8505c5439c6fdf800751f8be16c7550b8a4f75d5
Sergio Ferreira <sergio.ferreira@ctw.bmwgroup.com>
2025-09-16 11:38:40 +0100

    added some verbose to test the pipeline

--- lib/validations/agnostic/law-of-demeter.sh
+++ lib/validations/agnostic/law-of-demeter.sh
@@ -2,6 +2,10 @@
 #!/usr/bin/env bash
 
 find_lines_that_violate_lod() {
+
+  print_verbose "  - Checking for Law of Demeter violations..."
+  print_verbose "  - files: $1"
+
   local -n batch="$1";
   grep -nE '\b[a-zA-Z_][a-zA-Z0-9_]*(\.[a-zA-Z_][a-zA-Z0-9_]*){3,}\b' -- "${batch[@]}" \
     | grep -vE 'using ' \
