dd536b112bd8a462bc11d52462df10f01de6bbe9
Sergio Ferreira <sergio.ferreira@ctw.bmwgroup.com>
2025-09-17 11:35:37 +0100

    re-enable dart validations

--- lib/validations/dart/pump-and-settle-without-duration.sh
+++ lib/validations/dart/pump-and-settle-without-duration.sh
@@ -4,8 +4,8 @@ function _find_pump_and_settle_without_duration() {
     find_text_in_test 'tester.pumpAndSettle()' "$DIR" "$BASE_BRANCH" "$CURRENT_BRANCH"
 }
 
-#register_test_validation \
-#    "pump-and-settle-without-duration" \
-#    "LOW" \
-#    "_find_pump_and_settle_without_duration" \
-#    "PumpAndSettle without duration:"
+register_test_validation \
+    "pump-and-settle-without-duration" \
+    "LOW" \
+    "_find_pump_and_settle_without_duration" \
+    "PumpAndSettle without duration:"
